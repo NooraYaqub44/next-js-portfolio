d4b6fa90d04087177eaefbb702f9355c70a7dece
Noora <noorayaqub44@gmail.com>
2022-01-26 20:26:20 +0300

    Navbar created

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../styles/Theme'
+import Nav from '../components/Navabar'
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Nav/>
       <Component {...pageProps} />
     </ChakraProvider>
   )
